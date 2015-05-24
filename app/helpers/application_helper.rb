724087db3255617514e21d6cb291cbf5bb5b94e5
maangie <maangie906@gmail.com>
2015-05-24 17:41:45 +0900

    bootstrap 導入

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def stylesheet_link_tag_with_turbolinks
+    stylesheet_link_tag 'application', media: 'all',
+                        'data-turbolinks-track' => true
+  end
+
+  def collapse_button_tag(target, &block)
+    button_tag type: 'button', class: 'navbar-toggle',
+               data: { toggle: 'collapse', target: target }, &block
+  end
+
+  def url_for_twitter(user)
+    "https://twitter.com/#{user.nickname}"
+  end
 end
