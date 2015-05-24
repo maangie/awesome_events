947c5b5da4d41cf586e2ced5e07dae8542919dcf
maangie <maangie906@gmail.com>
2015-05-24 20:11:40 +0900

    開催日タグをヘルパへ移動

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def date_select_now(f, column)
+    now = Time.zone.now
+    f.date_select(column,
+                  start_year: now.year, end_year: now.year + 1, prompt: true)
+  end
+
   def stylesheet_link_tag_with_turbolinks
     stylesheet_link_tag 'application', media: 'all',
                         'data-turbolinks-track' => true
