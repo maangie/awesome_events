d0ef101075e5e03827a7653ed3a8980eef13747d
maangie <maangie906@gmail.com>
2015-05-25 15:27:25 +0900

    モデルのテスト

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#name' do
+    context '空白のとき' do
+      let(:event) { Event.new(name: '') }
+
+      it 'valid でないこと' do
+        event.valid?
+        expect(event.errors[:name]).to be_present
+      end
+    end
+  end
 end
