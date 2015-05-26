eb324ac90f340ddf6f5f50a7f6896fa85ca250e6
maangie <maangie906@gmail.com>
2015-05-26 19:47:01 +0900

    ログインユーザが retire にアクセスしたときのテスト

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  context 'ログインユーザが retire にアクセスしたとき' do
+    before do
+      session[:user_id] = create(:user).id
+      get :retire
+    end
 
+    it 'render される' do
+      expect(response).to render_template :retire
+    end
+  end
+
+  pending '未ログインユーザが retire にアクセスしたとき'
+  pending 'destroy'
 end
