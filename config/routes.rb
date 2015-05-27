b3e5ab0cb696ddfce9ab336ecad455a0f0c41232
maangie <maangie906@gmail.com>
2015-05-27 13:02:53 +0900

    余分な空白削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#create'
-  get '/logout' => 'sessions#destroy', as: :logout  
+  get '/logout' => 'sessions#destroy', as: :logout
 
   resource :user, only: :destroy do
     get 'retire'
